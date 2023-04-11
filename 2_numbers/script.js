c82a4edee2f96605360a89920ca0c202ef8d58cc
LuisBianchine <luishdb@hotmail.com>
2023-04-10 21:00:32 -0300

    AULAS

---
+++ 2_numbers/script.js
@@ -0,0 +1,4 @@
+console.log(typeof 5);
+console.log(typeof 10.5);
+console.log(typeof -123);
+
---
+++ 3_operadores/script.js
@@ -0,0 +1,5 @@
+console.log(5 + 5);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log("resto da divisão de 5 / 2 = " + 5 % 2); //Resto da divisão
